55363a1e3d753e146b4ebf73834f50ff9bb7d3b4
Samuel Wright <wsright987@gmail.com>
2025-03-06 20:03:59 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -2,4 +2,6 @@
 set -e
 pip install --upgrade pip
 pip install -r requirements.txt
+apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2
 playwright install --with-deps
+

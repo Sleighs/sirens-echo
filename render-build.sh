a0cf0f8978808b326ebf0ad037c952480f4aefc4
Samuel Wright <wsright987@gmail.com>
2025-03-06 20:21:28 -0500

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -10,5 +10,8 @@
 # Install system dependencies
 apt-get update && apt-get install -y libnss3 libatk1.0-0 libatk-bridge2.0-0 libcups2 libasound2 libxrandr2 libxkbcommon-x11-0
 
+npm install 
+npm install @playwright/test 
+
 # Install Playwright browsers
 npx playwright install --with-deps
